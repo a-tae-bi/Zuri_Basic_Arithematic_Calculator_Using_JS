4af91d740ed46eddf3b15d107edb67b61f7002ce
Tabitha Ama Abeka <99736998+a-tae-bi@users.noreply.github.com>
2022-06-24 17:52:21 +0000

    Corrected code to ensure that the results of the arithmetic operations are displayed

--- basic_calculator.js
+++ basic_calculator.js
@@ -1,38 +1,31 @@
 //Build a basic arithmetic calculator without a frontend
 //The calculator should be able to perform basic operations like Addition, Subtraction, Multiplication, & Division
 
-var number1 = prompt("Enter the first number: ");
-var number2 = prompt("Enter the second number: ");
+const choice = prompt("Enter the arithmatic operation to perform. Enter + for addition,or - for substraction, or / for division and * for multiplication ")
 
-number1 = parseFloat(number1);
-number2 = parseFloat(number2);
+const number1 = parseFloat(prompt("Enter the first number: "));
+const number2 = parseFloat(prompt("Enter the second number: "));
 
-console.log("The sum of the two numbers is: ", number1 + number2);
+let result = 0;
 
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
-    
-console.log("Welcome to Programiz!");
-
-var number1 = prompt("Enter the first number: ");
-var number2 = prompt("Enter the second number: ");
-
-number1 = parseFloat(number1);
-number2 = parseFloat(number2);
-
-let result
-
-var choice = prompt("Enter the arithmatic operation to perform. Enter + for addition,or - for substraction, or / for division and * for multiplication ")
-
-if (choice == "+"){
-      result = document.write("The sum of the two numbers is: ", number1 + number2);
+if (isNaN(number1) || isNaN(number2)){
+      alert("Incorrect numbers enterred. Refresh the page and start again.");
+}
+else {
+      if (choice == "+"){
+            result = number1 + number2; 
+            window.alert("The sum of the two numbers (" + number1 + " , " + number2 + ") is: " + result); 
       } else if (choice == "-"){
-       result = document.write("The results of substraction of the two numbers is: ", number1 - number2);
-      }else if (choice == "/"){
-       result = document.write("The results of division of the two numbers is: ", number1 / number2);
-      }else if (choice == "*"){
-      result = document.write("The results of mulplication of the two numbers is: ", number1 * number2);
+            result = number1 - number2;
+            window.alert("The results of substracting the two numbers (" + number1 + " , " + number2 + ") is: " + result);
+      } else if (choice == "/"){
+            let result = number1 / number2;
+            window.alert("The results of dividing the two numbers (" + number1 + " , " + number2 + ") is: " + result);     
+      } else if (choice == "*"){
+            let result = number1 * number2;
+            window.alert("The results of multiplying the two numbers (" + number1 + " , " + number2 + ") is: " + result);    
       } else {
-      result = document.write("You chose an invalid operation. Start the program again");}
+            window.alert("You chose an invalid arithmetic operation. Start the program again");
+      }
+}
       
-      alert(result);
